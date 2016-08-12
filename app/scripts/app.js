d480b90cdc4c86ee2ad6e5b94e6478d20e96c403
annahuffman147 <annahuffman.plc@gmail.com>
2016-08-12 08:55:40 -0600

    initial commit with yeoman set up for AngularFire + ExpressJS generator

---
+++ app/scripts/app.js
@@ -0,0 +1,20 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name rockadminApp
+ * @description
+ * # rockadminApp
+ *
+ * Main module of the application.
+ */
+angular.module('rockadminApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.auth'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name rockadminApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the rockadminApp
+ */
+angular.module('rockadminApp')
+  .controller('MainCtrl', ["auth", "$scope", "$location", function (auth, $scope, $location) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+
+    $scope.logout = function () {
+      auth.$signOut();
+      console.log('logged out');
+      $location.path('/login');
+      $scope.authData = null;
+    };
+
+  }]);
